229fddd712a9192f2827df682ce8a49db8d5ea71
Juan Aguirre <j1927aguirre@gmail.com>
2020-11-16 16:55:13 -0500

    Set up JEST as TestRunner

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -14,22 +16,27 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'JD1927'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('JD1927');
+    expect(component.title).toEqual('JD1927');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('JD1927 app is running!');
+  describe('#getPortfolioName()', () => {
+    it(`Should get 'Portfolio: JD1927' when its called`, () => {
+      // Arrange
+      // Act
+      const result = component.getPortfolioName();
+      // Assert
+      expect(result).toEqual(`Portfolio: JD1927`);
+    });
   });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'JD1927';
+
+  constructor() {}
+
+  getPortfolioName(): string {
+    return `Portfolio: ${this.title}`;
+  }
 }
