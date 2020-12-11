a7725ba4cfdbc3700670fe35aa6dc621db0223d6
Juan Aguirre <j1927aguirre@gmail.com>
2020-12-10 22:39:26 -0500

    Add a new card to the HomeComponent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { REPO_TASK_MANAGMENT, URL_TASK_MANAGMENT } from 'src/app/shared/global.constants';
+import { Component } from '@angular/core';
+import { REPO_POKEDEX, REPO_TASK_MANAGMENT, URL_POKEDEX, URL_TASK_MANAGMENT } from 'src/app/shared/global.constants';
+import { onOpenWebsiteByURL } from 'src/app/shared/utils';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   public URL_TASK_MANAGMENT: string = URL_TASK_MANAGMENT;
   public REPO_TASK_MANAGMENT: string = REPO_TASK_MANAGMENT;
+  public URL_POKEDEX: string = URL_POKEDEX;
+  public REPO_POKEDEX: string = REPO_POKEDEX;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
   onOpenWebsite(url: string): void {
-    window.open(url);
+    onOpenWebsiteByURL(url);
   }
 
 }
