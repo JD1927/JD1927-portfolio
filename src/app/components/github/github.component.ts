ef0ea2c253fcd1c3a353048ed36a98e65c12fccc
Juan Aguirre <j1927aguirre@gmail.com>
2020-12-12 23:25:51 -0500

    Add fancy input search to the github repository list

--- src/app/components/github/github.component.ts
+++ src/app/components/github/github.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { delay } from 'rxjs/operators';
 import { USER } from 'src/app/shared/global.constants';
 import { GithubRepo } from 'src/app/shared/models/github.model';
@@ -13,22 +14,40 @@ import { onOpenWebsiteByURL } from 'src/app/shared/utils';
 export class GithubComponent implements OnInit {
 
   githubRepositories: GithubRepo[] = [];
+  githubRepositoriesFilter: GithubRepo[] = [];
+
+  githubSearch: FormControl = new FormControl('');
 
   constructor(private githubService: GithubService) { }
 
   ngOnInit(): void {
     this.getPublicRepositoriesByUser();
+    this.validateSearhInput();
+  }
+
+  validateSearhInput(): void {
+    this.githubSearch.valueChanges.subscribe((value) => {
+      this.searchRepositoryByName(value);
+    });
+  }
+
+  searchRepositoryByName(value: string): void {
+    if (value && value !== '') {
+      this.githubRepositories = this.githubRepositoriesFilter.filter((repo) => repo.name.match(value));
+    } else {
+      this.githubRepositories = this.githubRepositoriesFilter;
+    }
   }
 
   getPublicRepositoriesByUser(): void {
     this.githubService.getPublicRepositoriesByUser(USER)
     .pipe(
-      // delay(6000)
+      delay(3000)
     )
     .subscribe(
       (res) => {
         this.githubRepositories = [...res];
-        console.log(this.githubRepositories);
+        this.githubRepositoriesFilter = [...res];
       },
       (err) => {
         console.log(err);
